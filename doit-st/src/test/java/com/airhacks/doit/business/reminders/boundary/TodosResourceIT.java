5d19e8483cf645d91dc7c67cdd9eb39baeede6ca
Sebastian Basner <sebastian@are-you-ready.de>
2015-10-31 16:29:37 +0100

    changed test for new API

--- doit-st/src/test/java/com/airhacks/doit/business/reminders/boundary/TodosResourceIT.java
+++ doit-st/src/test/java/com/airhacks/doit/business/reminders/boundary/TodosResourceIT.java
@@ -2,9 +2,9 @@ package com.airhacks.doit.business.reminders.boundary;
 
 import static com.airhacks.rulz.jaxrsclient.JAXRSClientProvider.buildWithURI;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -23,11 +23,28 @@ public class TodosResourceIT {
    public JAXRSClientProvider provider = buildWithURI("http://localhost:8080/doit/api/todos");
 
    @Test
-   public void fetchToDos() {
+   public void crud() {
       final Response response = this.provider.target().request(MediaType.APPLICATION_JSON).get();
       assertThat(response.getStatus(), is(200));
-      final JsonObject payload = response.readEntity(JsonObject.class);
-      System.out.println("payload " + payload);
-      assertTrue(payload.getString("caption").startsWith("implement"));
+      final JsonArray allTodos = response.readEntity(JsonArray.class);
+      System.out.println("payload " + allTodos);
+      assertFalse(allTodos.isEmpty());
+
+      final JsonObject todo = allTodos.getJsonObject(0);
+      assertTrue(todo.getString("caption").startsWith("impl"));
+
+      //GET with id
+      final JsonObject dedicatedTodo = this.provider.target().
+            path("42").
+            request(MediaType.APPLICATION_JSON).
+            get(JsonObject.class);
+      assertTrue(dedicatedTodo.getString("caption").contains("42"));
+
+      final Response deleteResponse = this.provider.target().
+            path("42").
+            request(MediaType.APPLICATION_JSON).
+            delete();
+      assertThat(deleteResponse.getStatus(), is(204));
+
    }
 }
